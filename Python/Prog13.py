8bbf422e0c63a7f682c001c7918741213de5c229
Shoaib-Ur-Rahman <101692018+Shoaib-Ur-Rahman@users.noreply.github.com>
2026-04-04 16:07:07 +0530

    2

--- Python/Prog13.py
+++ Python/Prog13.py
@@ -4,3 +4,8 @@ for i in range(2):
     a.append(b)
 for r in range(len(a)-1,-1,-1):
     print(a[r])
+s=0
+for i in range(len(a)):
+    if a[i]%2==0:
+        s=s+a[i]
+print(f"Sum Of Even Numbers= {s}")
