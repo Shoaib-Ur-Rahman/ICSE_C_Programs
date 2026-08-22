e86d3060df3609d564b428663f79fddf9255a8f5
Shoaib-Ur-Rahman <101692018+Shoaib-Ur-Rahman@users.noreply.github.com>
2026-08-22 10:32:49 +0530

    Add Java binary search practice program

---
+++ Java/BinarySearchPractice.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class BinarySearchPractice
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a[] = {17, 31, 49, 55, 64, 78, 87};
+        int key;
+        int first = 0;
+        int last = 6;
+        int mid;
+        boolean found = false;
+
+        System.out.print("Enter number to search: ");
+        key = sc.nextInt();
+
+        while(first <= last)
+        {
+            mid = (first + last) / 2;
+
+            if(a[mid] == key)
+            {
+                System.out.println("Number found at index " + mid);
+                found = true;
+                break;
+            }
+            else if(key > a[mid])
+            {
+                first = mid + 1;
+            }
+            else
+            {
+                last = mid - 1;
+            }
+        }
+
+        if(found == false)
+        {
+            System.out.println("Number not found");
+        }
+    }
+}
